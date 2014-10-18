cad7d71b3def1a69315072bff0c19a0e36c45970
Pierre JdlF <pierre.jdlf@gmail.com>
2014-10-18 17:57:38 +0200

    simple transitions, random position on a grid

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 navilinks
 =========
+# use
+- you will need PHP 5 activated for the [miniProxy](https://github.com/joshdick/miniProxy)
+- just open the `http://localhost/navilinks` page on your own webserver. 
+- `http://localhost/navilinks/?u=www.wikipedia.org` to set starting link
--- navilinks.js
+++ navilinks.js
@@ -18,20 +18,33 @@ var PARAMS = getSearchParameters();
 ////////////////////////////////////////////////////////
 
 
-
+var W,H;
 var PROXYURL = "proxy/miniProxy.php/";
 var UIBLOCKED = false;
-
+var BLACKLIST = false;
+var BLACKLISTED = [
+  "youtube\.com",
+  "google\.com",
+  "apple\.com",
+  "facebook\.com",
+  "twitter\.com",
+  "amazon\.com",
+  "yahoo\.com",
+  "linkedin\.com",
+  "vimeo\.com",
+  "github\.com"
+];
 
 
 ////////////////////////////////////////////////////////
 // extract domain from url
 function getdomain(url) {
-  var doms = url.replace(/^https*:\/\//,"").split("/");
+  var doms = url.replace(/^https*:\/\//,"").toLowerCase().split("/");
   if(doms && doms.length>0) {
     var spl = doms[0].split(".");
-    spl.shift();
-    return spl.join(".").toLowerCase();
+    var ext = spl.pop();
+    var dom = spl.pop();
+    return dom+"."+ext;
   } else
     return "no-domain-?";
 };
@@ -39,7 +52,16 @@ function getdomain(url) {
 ////////////////////////////////////////////////////////
 // determine if we want the link
 function isgoodlink(link) {
-  return link && link.length>3 && /\./g.test(link);
+  var blcklisted = false;
+  if(BLACKLIST) {
+    BLACKLISTED.forEach(function(url,i) {
+      var rxp = new RegExp(url,"i");
+      blcklisted = blcklisted || rxp.test(link);
+    })
+    if(blcklisted)
+      console.log("Blacklisted:",link);
+  }
+  return link && link.length>3 && /\./g.test(link) && !blcklisted;
 };
 
 ////////////////////////////////////////////////////////
@@ -47,7 +69,9 @@ function isgoodlink(link) {
 function getiframelinks() {
   var links = [];
   $("#iframe").contents().find('a').each( function(i,d) {
+    var srgx = new RegExp("^.*"+PROXYURL,'i');
     var url = $(d).attr('href');
+    if(url) url = url.replace(srgx,"");
     if(isgoodlink(url))
       links.push({
         url: url,
@@ -74,47 +98,135 @@ function navigateto(nexturl) {
 };
 
 ////////////////////////////////////////////////////////
+// get [0,1]-[0,1] position based on index, total, and ration wanted
+function getPosition(k,total) {
+
+  var RATIO = 1.5; //w/h
+  var c = Math.ceil( Math.sqrt(total/RATIO) ); // number of cells to make a grid containing all
+  var nw = RATIO*c; // nw is the horizontal number of cell 
+  var nh = c;
+  var cellW = (W/nw);
+  var cellH = (H/nh)
+  var col = k%nw;
+  var row = Math.floor(k/nw);
+  var g = 0.15; //gutter < 0.5 !
+  var posx = cellW*col + cellW*(g+Math.random()*(1-g)) - 70;
+  var posy = cellH*row + cellH*(g+Math.random()*(1-g)) - 20;
+
+  // random pos (to test)
+  //var posx = Math.random()*W;
+  //var posy = Math.random()*H;
+  return {x:posx,y:posy};
+}
+
+////////////////////////////////////////////////////////
+//
+function clicklink(nextlink) {
+  console.log("clicked link:",nextlink);
+  $('#links .active').removeClass("active").addClass("past");
+  navigateto(nextlink);
+};
+
+////////////////////////////////////////////////////////
 // will be called whenever the iframe is fully loaded
 function frameloaded() {
   console.log("iFrame finished loading!");
 
   // make all present links to disapear
-  $('#links past').remove();
-  $('#links active').removeClass("active").addClass("past");
+  $('#simplelist ul').empty();
+  $('#links .past').remove();
+  $('#links').append($("<div class='plan arriving'>"));
 
   // add the fresh new links
   var links = getiframelinks();
+  console.log("Links:",links);
 
-  links.forEach(function(i,l) {
-    var newL = Handlebars.compile( $("#link-template").html() )(l);
+  links.forEach(function(l,i) {
+    var newL = Handlebars.compile( $("#linkcloud-template").html() )(l);
     var div = $(newL);
+    
     div.click(function(){
-      console.log("clicked link:",l);
-      navigateto(l.url);
+      clicklink(l.url);
     })
-    $('#links').append(div);
+    
+    //var x,y = getPosition(i,links.length);
+    var pos = getPosition(i,links.length);
+    div.css({
+      left: pos.x,
+      top: pos.y
+    });
+
+    // add galaxy
+    $('#links .plan.arriving').append(div);
+    
+    // add in simple list
+    var listelem = Handlebars.compile( $("#linklist-template").html() )(l);
+    $('#simplelist ul').append($("<li>").html(listelem));
+
   });
+
+  // at the end, make them appear
+  setTimeout(function() {
+    //console.log("now appearing.");
+    $('#links .plan.arriving').addClass("active").removeClass("arriving"),
+    1000
+  })
 
   UIBLOCKED = false;
 }
 
 
 ////////////////////////////////////////////////////////
+// mouse position will pan vie, but throttled
+function directPan(m) {
+  console.log("now panning:",m);
+  $("#links .plan.active").css({
+    left: -m.x + W/2,
+    top: -m.y + H/2
+  })
+};
+var throttlePan = _.throttle(directPan,200);
+
+
+////////////////////////////////////////////////////////
 // INPUT CHANGES
-$("#input").keypress(function(event) {
-  if(event.which==13) { // ENTER
-    var url = $(this).val();
-    console.log("Typed new url:",url);
-    if(isgoodlink(url)) {
-      navigateto(url);
-    } else {
-      console.log("bad url given");
+$( document ).ready(function() {
+
+  W = $(document).width();
+  H = $(document).height();
+  console.log("Window:",W,H);
+
+  $("#input").keypress(function(event) {
+    if(event.which==13) { // ENTER
+      var url = $(this).val();
+      console.log("Typed new url:",url);
+      if(isgoodlink(url)) {
+        navigateto(url);
+      } else {
+        console.log("bad url given");
+      }
     }
-  }
-})
-$('#toggleframe').change(function() {
-  console.log("Toggling iframe.");
-  //$(this).is(":checked")
-  $("#iframe").toggle();
+  })
+  // iframe toggle
+  $("#iframe").toggle($('#toggleframe').is(":checked"));
+  $('#toggleframe').change(function() { $("#iframe").toggle(); });
+
+  // list toggle
+  $("#simplelist").toggle($('#togglelist').is(":checked"));
+  $('#togglelist').change(function() { $("#simplelist").toggle(); });
+  $( "html" ).mousemove(function(event) {
+    var m = {};
+    m.x = event.clientX;
+    m.y = event.clientY;
+    //console.log("mouse:",mx,my);
+    //directPan(m);
+    //throttlePan(m);
+  });
+
+  // if set on GET, start with given url
+  if(PARAMS.u) navigateto(PARAMS.u);
+
 });
+
+
 
